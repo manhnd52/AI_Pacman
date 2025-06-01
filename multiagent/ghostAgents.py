79f85ba1c0cc30b0d274fc6a5007cef603eeef40
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-06-01 22:34:07 +0700

    A* search update (SuperGhost)

--- multiagent/ghostAgents.py
+++ multiagent/ghostAgents.py
@@ -19,6 +19,7 @@ import random
 from util import manhattanDistance
 import util
 import heapq
+from math import ceil, floor
 
 
 class GhostAgent(Agent):
@@ -114,71 +115,106 @@ class SuperGhost(GhostAgent):
     def heuristic(self, a, b):
         return abs(a[0] - b[0]) + abs(a[1] - b[1])
     
+    def nextIntPosition(self, pos, direction):
+        if direction == (0, -1):
+            return pos[0], ceil(pos[1] - 1)  # South
+        elif direction == (0, 1):
+            return pos[0], floor(pos[1] + 1)  # North
+        elif direction == (-1, 0):
+            return ceil(pos[0] - 1), pos[1]
+        elif direction == (1, 0):
+            return floor(pos[0] + 1), pos[1]
+
+    
     def getDistribution(self, state):
         ghostState = state.getGhostState(self.index)
         legalActions = state.getLegalActions(self.index)
+        legalActionsVector = [Actions.directionToVector(a) for a in legalActions]
+        # print(legalActions)  # Loại bỏ hành động dừng lại
         pos = state.getGhostPosition(self.index)
         isScared = ghostState.scaredTimer > 0
         pacmanPosition = state.getPacmanPosition()
+        # print(f"Ghost index: {self.index} Ghost Position: {pos} Pacman Position: {pacmanPosition}")
 
-        bestActions = []
-
+        conf = ghostState.configuration
+        reverse = Actions.reverseDirection(conf.direction)
+        # print("Reverse: " + reverse)
+        reverse = Actions.directionToVector(reverse)
+        
         speed = 1
         if isScared:
             speed = 0.5
 
 
         # Sử dụng thuật toán A* để tìm đường đi đến Pacman
-        
         open_set = []
         visited = set()
+        ghostPos = state.getGhostPositions()
         walls = state.getWalls()
+        # for pos in ghostPos:
+        #     walls[int(pos[0])][int(pos[1])] = 1  # Đánh dấu vị trí của ghost là tường
+
         heapq.heappush(open_set, (self.heuristic(pos, pacmanPosition), 0, []))
+
+        bestAction = "hello"  # Mặc định là không di chuyển
 
         while open_set:
             f, g, path = heapq.heappop(open_set)
             current = path[-1] if path else pos  # Lấy node hiện tại từ path, nếu path rỗng thì dùng ghost_pos
-
             if current == pacmanPosition:
                 # Khi tìm ra được đường đi đến target, lấy hướng đi tiếp theo bằng cách lấy node đầu tiên trong path - vị trí hiện tại của ghost
                 if path:
                     move = (path[0][0] - pos[0], path[0][1] - pos[1])
-                    bestActions = [a for a in legalActions if Actions.directionToVector(a, speed) == move]
+                    bestAction = Actions.vectorToDirection(move)
                     # print(path)
-                    # print("Best actions found:", bestActions)
-                else:
-                    bestActions = Directions.STOP
+                    # print("$Best actions found:", bestAction)
+                    break
             
             if current in visited:
                 continue # Nhảy qua vòng lặp nếu đã thăm node này
             visited.add(current)
 
-
             rows, cols = walls.height, walls.width
-
             directions = {
-                (-1, 0): "up",
-                (1, 0): "down",
-                (0, -1): "left",
-                (0, 1): "right"
+                (-1, 0): "west",
+                (1, 0): "east",
+                (0, -1): "south",
+                (0, 1): "north"
             }
+            # print("Current node:", current, end="->")
             for d in directions:
-                nx, ny = int(current[0] + d[0]), int(current[1] + d[1])
-                if 0 <= nx < rows and 0 <= ny < cols and walls[nx][ny] == 0:
+                if d not in legalActionsVector and current == pos:  # Tránh di chuyển ngược lại hướng hiện tại
+                    continue
+                nx, ny = self.nextIntPosition(current, d)
+                nx = int(nx)
+                ny = int(ny)
+                # print(nx, ny, end=" ")
+                if 0 < nx < cols and 0 < ny < rows and not walls[nx][ny] :
                     next_node = (nx, ny)
                     if next_node not in visited:
+                        # print(next_node, end=" ")
                         heapq.heappush(open_set, (
                             g + 1 + self.heuristic(next_node, pacmanPosition),
                             g + 1,
                             path + [next_node]
                         ))
+            # print()
 
         # Construct distribution
-        bestProb = self.prob_scaredFlee if isScared else self.prob_attack
         dist = util.Counter()
-        for a in bestActions:
-            dist[a] = bestProb / len(bestActions)
-        for a in legalActions:
-            dist[a] += (1-bestProb) / len(legalActions)
-        dist.normalize()
+        if isScared:
+            if (len(legalActions) ==1):
+                dist[bestAction] = 1
+            else:
+                for a in legalActions:
+                    if a != bestAction:
+                        dist[a] = 1/(len(legalActions) - 1)
+
+        else:
+            dist[bestAction] = 1
+
+        # print(legalActions)
+        if bestAction not in legalActions:
+            input()
+          # Debugging pause
         return dist
